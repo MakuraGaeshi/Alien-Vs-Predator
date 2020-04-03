d2bf0c90f6257bbdad054cfa5c38335bdc19053f
Ogliss <randomdragon2@hotmail.com>
2020-04-03 19:37:36 +0100

    Options
    disabled options for Humans and Yautja, so they will Always be viable hosts

--- Source/RRYautja/util/AvPSettings.cs
+++ Source/RRYautja/util/AvPSettings.cs
@@ -142,7 +142,7 @@ namespace RRYautja.settings
                 }
                 */
                 settings.RaceKeyPairs.TryGetValue(td.defName, out bool setting);
-                Widgets.CheckboxLabeled(new Rect(x, num2, hostRect.width, 20f), text, ref setting);
+                Widgets.CheckboxLabeled(new Rect(x, num2, hostRect.width, 20f), text, ref setting, (td == ThingDefOf.Human|| td == YautjaDefOf.RRY_Alien_Yautja));
                 settings.RaceKeyPairs.SetOrAdd(td.defName, setting);
                 num2 += 20f;
             }
